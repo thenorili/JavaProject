511a7e7b314170a47793f03a87d30f7aa8576c90
Nori Li <norilinoriginal@gmail.com>
2020-06-03 19:25:19 +0000

    Co-authored-by: John Peter McGrath <mrjbrpg@gmail.com> Co-authored-by: greenjamc <tasbgt3ccc@gmail.com> Co-authored-by: JG Mathias <jg.mathias@yahoo.com>

--- Main.java
+++
@@ -1,5 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Get started on the application");
-    }
-}
---
+++ _Project/src/LocationScoreUI.java
@@ -0,0 +1,19 @@
+package _Project.src;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Timer;
+import java.util.TimerTask;
+import javax.swing.*;
+
+
+public class LocationScoreUI extends JFrame implements ActionListener, MouseListener
+{
+    public static final int WIDTH = 500;
+    public static final int HEIGHT = 500;
+}
