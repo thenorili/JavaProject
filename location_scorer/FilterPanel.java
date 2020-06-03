94b5edd2ca6682d53635b3360eb6ec5ee9b2a9f0
John Peter McGrath <mrjbrpg@gmail.com>
2020-06-03 15:42:14 -0700

    Implemented rough GUI elements.

--- location_scorer/FilterPanel.java
+++ location_scorer/FilterPanel.java
@@ -4,5 +4,24 @@ import javax.swing.*;
 import java.awt.*;
 
 public class FilterPanel extends JPanel{
+
+    private JComboBox dropDownState;
+    private JButton refreshButton;
+    FilterPanel(){
+        setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
+
+        dropDownState = new JComboBox<String>();
+        refreshButton = new JButton("Refresh");
+
+        add(dropDownState);
+        add(Box.createHorizontalGlue());
+        add(refreshButton);
+        repaint();
+        setVisible(true);
+
+
+
+
+    }
     
 }
